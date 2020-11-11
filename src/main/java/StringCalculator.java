19e769f30c45fa969700f7c47dbe8af6f3b8367c
Sarnava Konar <sarnava.9.lfc@gmail.com>
2020-11-11 14:06:40 +0530

    initial commit

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,5 +1,47 @@
 public class StringCalculator {
+
     public int add(String numbers) {
-        return 0;
+
+        int sum = 0;
+        StringBuilder negatives= new StringBuilder();
+        if(numbers.isBlank())
+            return sum;
+
+        if(numbers.startsWith("//")){
+
+            numbers = numbers.substring(2); //   ###\n1###2###3
+            String separator = numbers.split("\n")[0]; //    ###
+            numbers = numbers.split("\n")[1]; //    1###2###3
+            String[] numbersInt = numbers.split(separator);
+            for (String s : numbersInt) {
+                int number = Integer.parseInt(s);
+                if (number < 0) {
+                    negatives.append(number).append(",");
+                } else if (number <= 1000) {
+                    sum += number;
+                }
+            }
+        }
+        else {
+
+            numbers = numbers.replaceAll("\n",",");
+
+            String[] numbersInt = numbers.split(",");
+            for (String s : numbersInt) {
+                int number = Integer.parseInt(s);
+
+                if (number < 0) {
+                    negatives.append(number).append(",");
+                } else if (number <= 1000) {
+                    sum += number;
+                }
+            }
+        }
+
+        if(!negatives.toString().isBlank()){
+            throw new RuntimeException("negatives not allowed: "+negatives.substring(0, negatives.length()-1));
+        }
+
+        return sum;
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,11 +1,52 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StringCalculatorTest {
     @Test
     public void testAddShouldReturn0() {
         StringCalculator calculator = new StringCalculator();
         assertEquals(0, calculator.add(""));
+    }
+
+    @Test
+    public void testAddShouldReturn3() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(3, calculator.add("1,2"));
+    }
+
+    @Test
+    public void testAddShouldReturnMany() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(36, calculator.add("1,2,3,4,5,6,7,8"));
+    }
+
+    @Test
+    public void testAddShouldReturnSumWithNewLineCharacter() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(6, calculator.add("1\n2,3"));
+    }
+
+    @Test
+    public void testAddShouldReturnSumWithNewLineCharacter2() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(6, calculator.add("//###\n1###2###3")); //  //;\n1;2   //;\n1;2;3   //+\n1+2+3  //---\n1---2---3   //,\n1,2,3
+    }
+
+    @Test
+    public void testAddShouldReturnException() {
+        StringCalculator calculator = new StringCalculator();
+
+        Throwable exceptionThatWasThrown = assertThrows(Exception.class, () -> {
+            calculator.add("1,4,-1");
+        });
+        assertEquals(exceptionThatWasThrown.getMessage(), "negatives not allowed: -1");
+    }
+
+    @Test
+    public void testAddShouldIgnoreBigNums() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(2, calculator.add("2,1001"));
     }
 }
